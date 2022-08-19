7f48fe765f9f5e5866c3b95f8b0417e13cc9de85
S <30459885+tacticstactics@users.noreply.github.com>
2022-08-19 10:09:47 +0900

    11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,36 +4,62 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Text;
+using Thorlabs.CCS_Series.interop;
+
 using Thorlabs.CCS_Series;
 
 
-namespace CCS100_CSConsole
+namespace TLCCSCSConsole
 {
 
     public class Program
     {
 
-        #region Fields
-        private static TLCCS instrument = new TLCCS(IntPtr.Zero);
-        #endregion
-
-
 
         static void Main(string[] args)
         {
 
+            Console.WriteLine("TLCCS");
+
+
+        string instrumentNumber = "0x8089";
+        string resourceName = "USB0::0x1313::" + instrumentNumber + "::M" + "00233299" + "::RAW";
+
+           private TLCCS ccsSeries1;
+
+        private device1 = new TLCCS(resourceName, false, false);
+
+
+    }
+
+
+
+    // get parameters from command line
+    // build device list
+
+
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception raised by BuildDeviceList {0}", ex);
+                Console.ReadKey();
+                return;
+            }
+
+
 
 
         }
 
+    
+}
 
+//        private TLCCS ccsSeries;
 
-        private TLCCS ccsSeries;
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
+  //      public Form1()
+    //    {
+      //      InitializeComponent();
+       // }
 
 //        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
 //        {
@@ -42,16 +68,16 @@ namespace CCS100_CSConsole
 //                ccsSeries.Dispose();
 //        }
 
-        private void button_StartScan_Click(object sender, EventArgs e)
-        {
-            if (textBox_SerialNumber.Text.Length == 0)
-            {
-                MessageBox.Show("Please insert the 8 numerics of the serial number");
-                return;
-            }
+//        private void button_StartScan_Click(object sender, EventArgs e)
+ //       {
+ //           if (textBox_SerialNumber.Text.Length == 0)
+ //           {
+ //               MessageBox.Show("Please insert the 8 numerics of the serial number");
+ //               return;
+ //           }
 
             // set the busy cursor
-            this.Cursor = Cursors.WaitCursor;
+//            this.Cursor = Cursors.WaitCursor;
 
             // connect the ccs device and start the sample c application. Read out the instrument resource name from the sample application
             // the instrument number 0x8081 is used for the CCS100 device. If you have another CCS instrument please change the number.
@@ -60,44 +86,41 @@ namespace CCS100_CSConsole
             // 0x8085: CCS150
             // 0x8087: CCS175
             // 0x8089: CCS200
-            string instrumentNumber = "0x8089";
-            string resourceName = "USB0::0x1313::" + instrumentNumber + "::M" + textBox_SerialNumber.Text.ToString() + "::RAW";
+//            string instrumentNumber = "0x8089";
+//            string resourceName = "USB0::0x1313::" + instrumentNumber + "::M" + textBox_SerialNumber.Text.ToString() + "::RAW";
 
             // initialize device with the resource name (be sure the device is still connected)
-            ccsSeries = new TLCCS(resourceName, false, false);
+//            ccsSeries = new TLCCS(resourceName, false, false);
 
-            int status;
-            int res = ccsSeries.getDeviceStatus(out status);
+//            int status;
+//            int res = ccsSeries.getDeviceStatus(out status);
 
             // set the integration time
-            res = ccsSeries.setIntegrationTime((double)numericUpDown_IntegrationTime.Value);
+//            res = ccsSeries.setIntegrationTime((double)numericUpDown_IntegrationTime.Value);
 
             // start the scan
-            res = ccsSeries.startScan();
+//            res = ccsSeries.startScan();
 
-            if (res == 0)
-            {
+//            if (res == 0)
+//            {
                 // color the button green
-                button_StartScan.BackColor = System.Drawing.Color.LightGreen;
-                button_StartScan.Enabled = false;
-            }
+//                button_StartScan.BackColor = System.Drawing.Color.LightGreen;
+//                button_StartScan.Enabled = false;
+//            }
 
             // reset the cursor
-            this.Cursor = Cursors.Default;
+//            this.Cursor = Cursors.Default;
 
             // has the device started?
-            res = ccsSeries.getDeviceStatus(out status);
+//            res = ccsSeries.getDeviceStatus(out status);
 
             // camera has data available for transfer
-            if ((status & 0x0010) > 0)
-            {
-                double[] data = new double[3648];
-                res = ccsSeries.getScanData(data);
-            }
+//            if ((status & 0x0010) > 0)
+//            {
+//                double[] data = new double[3648];
+//                res = ccsSeries.getScanData(data);
+ //           }
 
-        }
-    }
-}
 
 
 
